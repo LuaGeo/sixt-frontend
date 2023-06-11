b4879957054b4b62328ab09b50e0611c3f25cc77
luageo <ldeoliveiratech@gmail.com>
2023-06-11 18:54:01 +0200

    ready to delivery

--- src/components/BackofficeModal.jsx
+++ src/components/BackofficeModal.jsx
@@ -41,47 +41,61 @@ export const BackofficeModal = ({
             }}
           ></button>
         </div>
-        <div className="prixDetails">
-          <h1 style={{ color: "black" }}>PÉRIODE DE LOCATION</h1>
-          <div className="periode">
-            <p>
-              Duré de location ({reservation.durationInDays} jours x{" "}
-              {reservation.vehicleDetails.priceDetails.dayPrice} )
-            </p>
 
-            <p>
-              €{" "}
-              {(
-                reservation.durationInDays *
-                reservation.vehicleDetails.priceDetails.dayPrice
-              ).toFixed(2)}
+        <div className="priceAndClientDetailsContainer">
+          <div className="prixDetails">
+            <h1 style={{ color: "black" }}>PÉRIODE DE LOCATION</h1>
+            <div className="periode">
+              <p>
+                Duré de location ({reservation.durationInDays} jours x{" "}
+                {reservation.vehicleDetails.priceDetails.dayPrice} )
+              </p>
+
+              <p>
+                €{" "}
+                {(
+                  reservation.durationInDays *
+                  reservation.vehicleDetails.priceDetails.dayPrice
+                ).toFixed(2)}
+              </p>
+            </div>
+            <h1 style={{ color: "black" }}>PROTECTIONS ET OPTIONS</h1>
+            <div className="options">
+              {reservation.vehicleDetails.priceDetails.options.map(
+                (option, index) => (
+                  <div key={index}>
+                    <p>{option.optionTitles}</p>
+                    <p>{option.optionPrices}</p>
+                  </div>
+                )
+              )}
+            </div>
+            <h1 style={{ color: "black" }}>FRAIS</h1>
+            <div className="options">
+              {reservation.vehicleDetails.priceDetails.fees.map(
+                (option, index) => (
+                  <div key={index}>
+                    <p>{option.feeTitles}</p>
+                    <p>{option.feePrices}</p>
+                  </div>
+                )
+              )}
+            </div>
+            <div className="total">
+              <p>TOTAL</p>
+              <p>{reservation.totalCost}</p>
+            </div>
+          </div>
+          <div className="clientInfos">
+            <p style={{ textDecoration: "underline", marginBottom: "10px" }}>
+              Infos Client
             </p>
-          </div>
-          <h1 style={{ color: "black" }}>PROTECTIONS ET OPTIONS</h1>
-          <div className="options">
-            {reservation.vehicleDetails.priceDetails.options.map(
-              (option, index) => (
-                <div key={index}>
-                  <p>{option.optionTitles}</p>
-                  <p>{option.optionPrices}</p>
-                </div>
-              )
-            )}
-          </div>
-          <h1 style={{ color: "black" }}>FRAIS</h1>
-          <div className="options">
-            {reservation.vehicleDetails.priceDetails.fees.map(
-              (option, index) => (
-                <div key={index}>
-                  <p>{option.feeTitles}</p>
-                  <p>{option.feePrices}</p>
-                </div>
-              )
-            )}
-          </div>
-          <div className="total">
-            <p>TOTAL</p>
-            <p>{reservation.totalCost}</p>
+            <p>
+              Nom complet :{" "}
+              {reservation.clientDetails.firstName.charAt(0).toUpperCase()}{" "}
+              {reservation.clientDetails.lastName.toUpperCase()}
+            </p>
+            <p>Email : {reservation.clientDetails.email}</p>
           </div>
         </div>
       </div>
